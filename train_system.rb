7730e2f08a528674dd7dd2cd3921a024ac3b554c
Emily Behlmann <ebehlmann@gmail.com>
2014-10-13 22:56:28 -0500

    Added line-add for operators

--- train_system.rb
+++ train_system.rb
@@ -1,5 +1,7 @@
 require 'active_record'
 require './lib/station'
+require './lib/line'
+require './lib/stop'
 
 database_configurations = YAML::load(File.open('./db/config.yml'))
 development_configuration = database_configurations['development']
@@ -53,6 +55,16 @@ def operator_menu
 	end
 end
 
+def booleanerator(submission)
+	if submission == 'y'
+		true
+	elsif submission == 'n'
+		false
+	else
+		puts "Sorry, that's not a valid input."
+	end
+end
+
 def train_station_add
 	puts "What's the name of the station?"
 	station_name = gets.chomp
@@ -63,17 +75,30 @@ def train_station_add
 	puts "Does it have a restaurant? Type 'y' for yes or 'n' for no."
 	restaurant_answer = gets.chomp
 
-	if restaurant_answer == 'y'
-		restaurant_boolean = true
-	elsif restaurant_answer == 'n'
-		restaurant_boolean = false
-	else
-		puts "Sorry, that wasn't a valid submission."
-	end
+	restaurant_boolean = booleanerator(restaurant_answer)
 
 	station = Station.new({name: station_name, location: location, opens: open_time, restaurant: restaurant_boolean})
 	station.save
 	puts "#{station_name} has been added."
+end
+
+def line_add
+	puts "What's the name of the line?"
+	line_name = gets.chomp
+	puts "What's the price in dollars to ride the full line?"
+	price = gets.chomp
+	puts "How many seats are available?"
+	seats = gets.chomp
+	puts "Can riders check bags? Type 'y' for yes or 'n' for no."
+	bags_answer = gets.chomp
+	bags_boolean = booleanerator(bags_answer)
+	puts "Are meals served on board? Type 'y' for yes or 'n' for no."
+	meals_answer = gets.chomp
+	meals_boolean = booleanerator(meals_answer)
+
+	line = Line.new({name: line_name, price: price, seats: seats, bagcheck: bags_boolean, meals: meals_boolean})
+	line.save
+	puts "The #{line_name} line has been added."
 end
 
 welcome
